c6fb0a567f8449395aec64b5611bb39d04a8d3a9
Vakil Verma <vakilverma7@gmail.com>
2026-03-15 15:42:18 +0530

    env updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://jobskills:job123@cluster0.vzimmpt.mongodb.net/jobportal");
+    await mongoose.connect("mongodb+srv://jobskills:job123@cluster0.vzimmpt.mongodb.net/jobportal");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error.message);
